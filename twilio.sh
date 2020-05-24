cabf478375e3be9da817a84d2a5d292fa29d98e1
Ubuntu <ubuntu@ip-172-31-10-92.us-west-2.compute.internal>
2020-05-24 02:31:07 +0000

    Update docs: add documetation to twilio.sh

--- twilio.sh
+++ twilio.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+
+#Generic script to send messages to a mobile phone
+#   - loads ENV variables for security purposes
+#       - TWILIO_ACCOUNT_SID
+#       - TWILIO_AUTH_TOKEN
+#       - TWILIO_NUMBER_TO_SEND #number you want the message sent to 
+#       - TWILIO_NUMBER #Number in your tewilio account
+#   - Accepts 1 paramter that is the message you would like to send
+#   - Send the message using twilio
+
+#load env vars
 source ~/.bash_profile
+
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin:/home/ubuntu/monitoringTools
 
-# usage: twilio.sh 'Hi there, your new phone number is working.'
-# load env variables and use for security
+
 
 #message given to send
 msg=$1
 account_sid=$TWILIO_ACCOUNT_SID
 auth_token=$TWILIO_AUTH_TOKEN
-wades_number=$TWILIO_WADES_NUMBER
+number=$TWILIO_NUMBER_TO_SEND
 twilio_number=$TWILIO_NUMBER
 
 
@@ -17,9 +28,9 @@ twilio_number=$TWILIO_NUMBER
     
    
 
-#send to wade
+#send text message
 curl --silent --output /dev/null -X POST -d "Body=${msg}" \
-    -d "From=${twilio_number}" -d "To=${wades_number}" \
+    -d "From=${twilio_number}" -d "To=${number}" \
     "https://api.twilio.com/2010-04-01/Accounts/${account_sid}/Messages" \
     -u "${account_sid}:${auth_token}"
 
